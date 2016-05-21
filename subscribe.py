72084989d96d47e24fdd3cb83009ac0670b486dd
Tom Gillespie <tgbugs@gmail.com>
2016-05-21 08:13:39 -0700

    zendesk support working

--- subscribe.py
+++ subscribe.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python3.5
 
+import os
 from os import environ
 import asyncio
-import certifi
-import websockets
 import ssl
 import uuid
 import json
+
+import certifi
+import websockets
 import robobrowser
+from zdesk import Zendesk
+
 from IPython import embed
 
-#USERNAME=
-#PASSWORD=
 
 def _ssl_context(verify=True):
     ssl_context = ssl.create_default_context(cafile=certifi.where())
@@ -46,6 +48,58 @@ async def process_messages(websocket, handler):
             pass
 
 
+def generic_incontext_handler(message):
+    return message['payload'][0]['links']['incontext']
+
+
+def slack_handler(message):
+    pass
+
+
+class slackHandler:
+    def __init__(self, api_token):
+        self.api_token = api_token
+    
+    def connect(self):
+        pass
+
+
+# zendesk
+with open(os.path.expanduser('~/files/zendeskinfo.json'), 'rt') as f:
+    zendeskinfo = json.load(f)
+zendesk = Zendesk(**zendeskinfo)
+
+def zdesk_handler(message):
+    data = message['payload'][0]
+    #exact = data['target'][0]['selector'][3]['exact']
+    exact = [i['exact'] for o in data['target'] for i in o['selector'] if 'exact' in i][0]
+    href = data['links']['incontext']
+    new_ticket = {                                 
+        'ticket': {
+            'requester': {
+                'name': 'hypothesis bot',
+                'email': zendeskinfo['zdesk_email'],
+            },
+            'subject':'hypothes.is annotation on {} by {}'.format(data['title'], data['user'].split(':',1)[1]),
+            'description': '"{}" | {} {}'.format(exact, data['text'], href) , 
+            'tags': data['tags'],
+            #'ticket_field_entries': [
+                #{
+                    #'ticket_field_id':24394906,  # referrer
+                    #'value': 'hypothes.is {}'.format(data['user'].split(':',1)[1])  # hypothes.is
+                #},
+                #{
+                    #'ticket_field_id':24394926,  # url
+                    #'ticket_field_id':'Url',  # url
+                    #'value': data['links']['incontext']  # incontext_url
+                #}
+            #]
+        }
+    }
+    result = zendesk.ticket_create(data=new_ticket)
+    print(result)
+
+
 class Handler:
     def __init__(self, post_filter_rules):
         pass
@@ -53,27 +107,23 @@ class Handler:
     def process(self, message):
         if message['type'] == 'annotation-notification':
             print('ANNOTATION')
+            #print(generic_incontext_handler(message))
+            zdesk_handler(message)
             print(message)
+            print()
         else:
             print('NOT ANNOTATION')
             print(message)
 
-
-def get_auth_header(username, password):
-    url = 'https://hypothes.is/login'
-    br = robobrowser.RoboBrowser()
-    br.open(url)
-    form = br.get_form(id=0)
-    print(form)
-    form['username'].value = username
-    form['password'].value = password
-    br.submit_form(form)
-    session_cookie = br.session.cookies['session']
-    return {'Cookie': 'session=%s' % session_cookie}
+        #embed()
 
 
 class preFilter:
-    def __init__(self, groups=['__world__'], users=[], uris=[], tags=[],
+    """ Create a filter that will run on the hypothes.is server
+        Make group empty to default to allow all groups the authed user
+        is a member of in the hypothes.is system.
+    """
+    def __init__(self, groups=[], users=[], uris=[], tags=[],
                  create=True, update=True, delete=True,
                  match_policy='include_all'):
 
@@ -90,7 +140,7 @@ class preFilter:
         self.tags = tags
 
         self.clause_map = [
-            ('/group', self.groups),
+            ('/group', self.groups),  # __world__
             ('/users', self.users),
             ('/uri', self.uris),
             ('/tags', self.tags),
@@ -127,6 +177,19 @@ class preFilter:
         return output
 
 
+def get_auth_header(username, password):
+    url = 'https://hypothes.is/login'
+    br = robobrowser.RoboBrowser()
+    br.open(url)
+    form = br.get_form(id=0)
+    print(form)
+    form['username'].value = username
+    form['password'].value = password
+    br.submit_form(form)
+    session_cookie = br.session.cookies['session']
+    return {'Cookie': 'session=%s' % session_cookie}
+
+
 async def ws_loop():
     websocket_endpoint = 'wss://hypothes.is/ws'
     post_filter_rules = None
@@ -140,6 +203,7 @@ async def ws_loop():
     password = environ.get('HYPUSH_PASSWORD', 'PASSWORD')
     api_token = environ.get('HYPUSH_API_TOKEN', 'TOKEN')
     groups = environ.get('HYPUSH_GROUPS', '__world__').split(' ')
+    print(groups)
 
     #headers = {'Authorization': 'Bearer ' + api_token} #, 'Content-Type': 'application/json;charset=utf-8' }  # once the websocket auth goes in
     header = get_auth_header(username, password)
@@ -147,10 +211,10 @@ async def ws_loop():
 
     }
     extra_headers.update(header)
-    filters = preFilter(
+    filters = preFilter(groups=groups).export()
         #uris=['https://github.com/','https://hypothes.is/'],
-        uris=['https://knowledge-space.org/'],
-        groups=['__world__','47q2eVPy']).export()
+        #uris=['https://knowledge-space.org/'],
+        #groups=['__world__']).export()
 
     while True:
         try:
