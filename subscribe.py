f4c6e6b1682d3c4908dc50752f044ffb9de0287c
Tom Gillespie <tgbugs@gmail.com>
2016-05-21 05:48:37 -0700

    can now view group annotations using cookie auth, this will change to api_token on mondayish, added class for handling preFilter rules (run hypothesis serverside)

--- subscribe.py
+++ subscribe.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3.5
 
+from os import environ
 import asyncio
 import certifi
 import websockets
 import ssl
 import uuid
 import json
+import robobrowser
 from IPython import embed
 
+#USERNAME=
+#PASSWORD=
 
 def _ssl_context(verify=True):
     ssl_context = ssl.create_default_context(cafile=certifi.where())
@@ -55,21 +59,102 @@ class Handler:
             print(message)
 
 
+def get_auth_header(username, password):
+    url = 'https://hypothes.is/login'
+    br = robobrowser.RoboBrowser()
+    br.open(url)
+    form = br.get_form(id=0)
+    print(form)
+    form['username'].value = username
+    form['password'].value = password
+    br.submit_form(form)
+    session_cookie = br.session.cookies['session']
+    return {'Cookie': 'session=%s' % session_cookie}
+
+
+class preFilter:
+    def __init__(self, groups=['__world__'], users=[], uris=[], tags=[],
+                 create=True, update=True, delete=True,
+                 match_policy='include_all'):
+
+        self.create = create
+        self.update = update
+        self.delete = delete
+
+        #include_all include_any
+        self.match_policy = match_policy
+
+        self.groups = groups
+        self.users = users
+        self.uris = uris
+        self.tags = tags
+
+        self.clause_map = [
+            ('/group', self.groups),
+            ('/users', self.users),
+            ('/uri', self.uris),
+            ('/tags', self.tags),
+        ]
+
+    def _make_clauses(self):
+        clauses = []
+        for field, value in self.clause_map:
+            if value:
+                clauses.append(
+                    {'field':[field],
+                     'case_sensitive':True,
+                     'operator':'one_of',
+                     'options':{},
+                     'value':value,
+                    }
+                )
+
+        return clauses
+
+    def export(self):
+        output = {
+            'filter':{
+                'actions':{
+                    'create':self.create,
+                    'update':self.update,
+                    'delete':self.delete,
+                },
+                'match_policy':self.match_policy,
+                'clauses':self._make_clauses(),
+            },
+        }
+
+        return output
+
+
 async def ws_loop():
     websocket_endpoint = 'wss://hypothes.is/ws'
     post_filter_rules = None
     handler = Handler(post_filter_rules)
     ssl_context = _ssl_context(verify=True)
-    filters = {'filter': {
-        'match_policy': 'include_all',
-        'clauses': [],
-        'actions': {'create': True, 'update': True, 'delete': True},
-        }
+    
+    #post to login form
+    #requests.post(username passworld) response has a cookies
+    #copy the set cookie header into the connect
+    username = environ.get('HYPUSH_USERNAME', 'USERNAME')
+    password = environ.get('HYPUSH_PASSWORD', 'PASSWORD')
+    api_token = environ.get('HYPUSH_API_TOKEN', 'TOKEN')
+    groups = environ.get('HYPUSH_GROUPS', '__world__').split(' ')
+
+    #headers = {'Authorization': 'Bearer ' + api_token} #, 'Content-Type': 'application/json;charset=utf-8' }  # once the websocket auth goes in
+    header = get_auth_header(username, password)
+    extra_headers = {
+
     }
+    extra_headers.update(header)
+    filters = preFilter(
+        #uris=['https://github.com/','https://hypothes.is/'],
+        uris=['https://knowledge-space.org/'],
+        groups=['__world__','47q2eVPy']).export()
 
     while True:
         try:
-            async with websockets.connect(websocket_endpoint, ssl=ssl_context) as ws:
+            async with websockets.connect(websocket_endpoint, ssl=ssl_context, extra_headers=extra_headers) as ws:
                 await setup_connection(ws)
                 print('working!')
                 await setup_filters(ws, filters)
