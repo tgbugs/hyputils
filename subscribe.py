3bfa96391309f15dddd1c8c9d9c34412b50edc15
Tom Gillespie <tgbugs@gmail.com>
2016-05-25 18:39:10 -0700

    updated how filtering/handling is implemented to use a filterHandler class to link the two for post filtering

--- subscribe.py
+++ subscribe.py
@@ -6,6 +6,7 @@ import asyncio
 import ssl
 import uuid
 import json
+import yaml
 
 import certifi
 import websockets
@@ -48,74 +49,105 @@ async def process_messages(websocket, handler):
             pass
 
 
-def generic_incontext_handler(message):
-    return message['payload'][0]['links']['incontext']
+class filterHandler:
+    """ Base class that all filter handlers should be derived from.
+        Any authenticiation state needed to link services should be
+        managed in __init__. Sorta a crappy python ITTT.
+    """
 
+    def filter(self, message):
+        return True
 
-def slack_handler(message):
-    pass
+    def handler(self, message):
+        raise NotImplemented('You need to implement this in a subclass')
+
+    def __call__(self, message):
+        if self.filter(message):
+            self.handler(message)
 
 
 class slackHandler:
     def __init__(self, api_token):
         self.api_token = api_token
     
-    def connect(self):
+    def filter(self):
+        pass
+
+    def handler(self):
         pass
 
 
-# zendesk
-with open(os.path.expanduser('~/files/zendeskinfo.json'), 'rt') as f:
-    zendeskinfo = json.load(f)
-zendesk = Zendesk(**zendeskinfo)
+class zendeskHandler(filterHandler):
+    """ Handler that creates zendeks tickets. Currently hardcoding filter
+        rules (eg uri) but could make this configurabe if desired. May also
+        break these filterHandlers out into their own file that users could
+        create independently of the subscription setup here....
+    """
 
-def zdesk_handler(message):
-    data = message['payload'][0]
-    #exact = data['target'][0]['selector'][3]['exact']
-    exact = [i['exact'] for o in data['target'] for i in o['selector'] if 'exact' in i][0]
-    href = data['links']['incontext']
-    new_ticket = {                                 
-        'ticket': {
-            'requester': {
-                'name': 'hypothesis bot',
-                'email': zendeskinfo['zdesk_email'],
-            },
-            'subject':'hypothes.is annotation on {} by {}'.format(data['title'], data['user'].split(':',1)[1]),
-            'description': '"{}" | {} {}'.format(exact, data['text'], href) , 
-            'tags': data['tags'],
-            #'ticket_field_entries': [
-                #{
-                    #'ticket_field_id':24394906,  # referrer
-                    #'value': 'hypothes.is {}'.format(data['user'].split(':',1)[1])  # hypothes.is
-                #},
-                #{
-                    #'ticket_field_id':24394926,  # url
-                    #'ticket_field_id':'Url',  # url
-                    #'value': data['links']['incontext']  # incontext_url
-                #}
-            #]
+    def __init__(self, infopath='~/files/zendeskinfo.yaml'):
+        with open(os.path.expanduser(infopath), 'rt') as f:
+            self.zendeskinfo = yaml.load(f)
+        self.zendesk = Zendesk(**self.zendeskinfo)
+
+    def filter(self, message):
+        if message['options']['action'] != 'create':
+            print('Annotation was not a creation, run update path?')
+            return
+        data = message['payload'][0]
+        if 'scicrunch.org' in data['uri']:
+            return True
+
+    def handler(self, message):
+        data = message['payload'][0]
+        exact = [i['exact'] for o in data['target'] for i in o['selector'] if 'selector' in o and 'exact' in i]
+        exact = exact[0] if exact else 'No description provided'
+        href = data['links']['incontext']
+        new_ticket = {                                 
+            'ticket': {
+                'requester': {
+                    'name': 'hypothesis bot',
+                    'email': self.zendeskinfo['zdesk_email'],
+                },
+                'subject':'hypothes.is annotation on {} by {}'.format(data['document']['title'], data['user'].split(':',1)[1]),
+                'description': '"{}" | {} {}'.format(exact, data['text'], href) , 
+                'tags': data['tags'],
+                #'ticket_field_entries': [
+                    #{
+                        #'ticket_field_id':24394906,  # referrer
+                        #'value': 'hypothes.is {}'.format(data['user'].split(':',1)[1])  # hypothes.is
+                    #},
+                    #{
+                        #'ticket_field_id':24394926,  # url
+                        #'ticket_field_id':'Url',  # url
+                        #'value': data['links']['incontext']  # incontext_url
+                    #}
+                #]
+            }
         }
-    }
-    result = zendesk.ticket_create(data=new_ticket)
-    print(result)
+        result = self.zendesk.ticket_create(data=new_ticket)
+        print(result)
+
+
+class printHandler(filterHandler):
+    def filter(self, message):
+        return True
+
+    def handler(self, message):
+        print(message['payload'][0]['links']['incontext'])
+        print(message)
 
 
 class Handler:
-    def __init__(self, post_filter_rules):
-        pass
+    def __init__(self, filter_handlers):
+        self.filter_handlers = filter_handlers  # list of filterHandlers that should be run on every message
 
     def process(self, message):
         if message['type'] == 'annotation-notification':
-            print('ANNOTATION')
-            #print(generic_incontext_handler(message))
-            zdesk_handler(message)
-            print(message)
-            print()
+            for fh in self.filter_handlers:
+                fh(message)
         else:
             print('NOT ANNOTATION')
             print(message)
-
-        #embed()
 
 
 class preFilter:
@@ -136,7 +168,7 @@ class preFilter:
 
         self.groups = groups
         self.users = users
-        self.uris = uris
+        self.uris = uris  # NOTE: uri filters must be exact :(
         self.tags = tags
 
         self.clause_map = [
@@ -192,8 +224,8 @@ def get_auth_header(username, password):
 
 async def ws_loop():
     websocket_endpoint = 'wss://hypothes.is/ws'
-    post_filter_rules = None
-    handler = Handler(post_filter_rules)
+    filter_handlers = [zendeskHandler(), printHandler()]
+    handler = Handler(filter_handlers)
     ssl_context = _ssl_context(verify=True)
     
     #post to login form
@@ -205,16 +237,13 @@ async def ws_loop():
     groups = environ.get('HYPUSH_GROUPS', '__world__').split(' ')
     print(groups)
 
-    #headers = {'Authorization': 'Bearer ' + api_token} #, 'Content-Type': 'application/json;charset=utf-8' }  # once the websocket auth goes in
+    #headers = {'Authorization': 'Bearer ' + api_token}#, 'Content-Type': 'application/json;charset=utf-8'}  # once the websocket auth goes in
     header = get_auth_header(username, password)
     extra_headers = {
 
     }
     extra_headers.update(header)
-    filters = preFilter(groups=groups).export()
-        #uris=['https://github.com/','https://hypothes.is/'],
-        #uris=['https://knowledge-space.org/'],
-        #groups=['__world__']).export()
+    filters = preFilter().export()
 
     while True:
         try:
