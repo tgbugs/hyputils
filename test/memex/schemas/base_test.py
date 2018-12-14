e0f2a6bbdf645230a430b0cafd3ee1b7f4d09d45
Tom Gillespie <tgbugs@gmail.com>
2018-12-13 19:03:11 -0800

    memex removed tests for removed components

--- test/memex/schemas/base_test.py
+++ test/memex/schemas/base_test.py
@@ -7,15 +7,8 @@ import enum
 from mock import Mock
 import pytest
 
-import colander
-from pyramid.exceptions import BadCSRFToken
-
 from hyputils.memex.schemas import ValidationError
-from hyputils.memex.schemas.base import enum_type, CSRFSchema, JSONSchema
-
-
-class ExampleCSRFSchema(CSRFSchema):
-    pass
+from hyputils.memex.schemas.base import JSONSchema
 
 
 class ExampleJSONSchema(JSONSchema):
@@ -32,28 +25,6 @@ class ExampleJSONSchema(JSONSchema):
         },
         "required": [prop_name_type("foo"), prop_name_type("bar")],
     }
-
-
-class TestCSRFSchema(object):
-    def test_raises_badcsrf_with_bad_csrf(self, pyramid_request):
-        schema = ExampleCSRFSchema().bind(request=pyramid_request)
-
-        with pytest.raises(BadCSRFToken):
-            schema.deserialize({})
-
-    def test_ok_with_good_csrf(self, pyramid_request):
-        csrf_token = pyramid_request.session.get_csrf_token()
-        pyramid_request.POST["csrf_token"] = csrf_token
-        schema = ExampleCSRFSchema().bind(request=pyramid_request)
-
-        # Does not raise
-        schema.deserialize({})
-
-    def test_ok_with_good_csrf_from_header(self, pyramid_csrf_request):
-        schema = ExampleCSRFSchema().bind(request=pyramid_csrf_request)
-
-        # Does not raise
-        schema.deserialize({})
 
 
 class TestJSONSchema(object):
@@ -87,36 +58,3 @@ class TestJSONSchema(object):
         assert message.startswith(
             "'foo' is a required property, 'bar' is a required property"
         )
-
-
-class Color(enum.Enum):
-    red = "#ff0000"
-    green = "#00ff00"
-    blue = "#0000ff"
-
-
-class TestEnumType(object):
-    def test_serialize_returns_a_string(self, color_type):
-        node = Mock()
-        assert color_type.serialize(node, Color.red) == "red"
-
-    def test_serialize_returns_an_empty_string_if_value_is_none(self, color_type):
-        node = Mock()
-        assert color_type.serialize(node, None) == ""
-
-    def test_deserialize_returns_none_if_value_is_null(self, color_type):
-        node = Mock()
-        assert color_type.deserialize(node, colander.null) is None
-
-    def test_deserialize_returns_an_enum(self, color_type):
-        node = Mock()
-        assert color_type.deserialize(node, "red") == Color.red
-
-    def test_deserialize_raises_if_value_unknown(self, color_type):
-        node = Mock()
-        with pytest.raises(colander.Invalid):
-            color_type.deserialize(node, "rebeccapurple")
-
-    @pytest.fixture
-    def color_type(self):
-        return enum_type(Color)()
