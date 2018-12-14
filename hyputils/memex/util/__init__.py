afaf5b0b7ff1a4639d1246aff8a2fd5999263b63
Tom Gillespie <tgbugs@gmail.com>
2018-12-13 18:49:39 -0800

    util __init__ remove unused db import

--- hyputils/memex/util/__init__.py
+++ hyputils/memex/util/__init__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import unicode_literals
-from hyputils.memex.util import db
 from hyputils.memex.util import user
 
-__all__ = ("db", "user")
+__all__ = ("user",)
