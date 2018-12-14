6301741dd185a156be2b95db13db7efd0798abff
Tom Gillespie <tgbugs@gmail.com>
2018-12-13 20:55:31 -0800

    memex run import of test common

---
+++ test/common/factories/__init__.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+"""Factory classes for easily generating test objects."""
+from __future__ import unicode_literals
+
+from .base import set_session
+
+from .activation import Activation
+from .annotation import Annotation
+from .annotation_moderation import AnnotationModeration
+from .auth_client import AuthClient, ConfidentialAuthClient
+from .auth_ticket import AuthTicket
+from .authz_code import AuthzCode
+from .document import Document, DocumentMeta, DocumentURI
+from .feature import Feature
+from .flag import Flag
+from .group import Group, OpenGroup, RestrictedGroup
+from .group_scope import GroupScope
+from .organization import Organization
+from .setting import Setting
+from .token import DeveloperToken, OAuth2Token
+from .user import User
+from .user_identity import UserIdentity
+
+__all__ = (
+    "Activation",
+    "Annotation",
+    "AnnotationModeration",
+    "AuthClient",
+    "AuthTicket",
+    "AuthzCode",
+    "ConfidentialAuthClient",
+    "DeveloperToken",
+    "Document",
+    "DocumentMeta",
+    "DocumentURI",
+    "Feature",
+    "Flag",
+    "Group",
+    "GroupScope",
+    "OAuth2Token",
+    "OpenGroup",
+    "Organization",
+    "RestrictedGroup",
+    "Setting",
+    "User",
+    "UserIdentity",
+    "set_session",
+)
---
+++ test/common/factories/group.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+import factory
+
+from h import models
+from h.models.group import JoinableBy, ReadableBy, WriteableBy
+
+from .base import ModelFactory
+from .group_scope import GroupScope
+from .user import User
+
+
+class Group(ModelFactory):
+    class Meta:
+        model = models.Group
+        sqlalchemy_session_persistence = "flush"
+
+    name = factory.Sequence(lambda n: "Test Group {n}".format(n=str(n)))
+    authority = "example.com"
+    creator = factory.SubFactory(User)
+    joinable_by = JoinableBy.authority
+    readable_by = ReadableBy.members
+    writeable_by = WriteableBy.members
+    members = factory.LazyAttribute(lambda obj: [obj.creator])
+
+    @factory.post_generation
+    def scopes(self, create, scopes=0, **kwargs):
+        if isinstance(scopes, int):
+            scopes = [GroupScope(group=self) for _ in range(0, scopes)]
+
+        self.scopes = scopes or []
+
+
+class OpenGroup(Group):
+
+    name = factory.Sequence(lambda n: "Test Open Group {n}".format(n=str(n)))
+
+    joinable_by = None
+    readable_by = ReadableBy.world
+    writeable_by = WriteableBy.authority
+    members = []
+
+
+class RestrictedGroup(Group):
+    name = factory.Sequence(lambda n: "Test Restricted Group {n}".format(n=str(n)))
+
+    joinable_by = None
+    readable_by = ReadableBy.world
