01dc8e7432776cfea860d6d98d936d876de8ed5a
Tom Gillespie <tgbugs@gmail.com>
2019-03-30 16:57:42 -0400

    setup manifest and travis updated

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@ from setuptools import setup
 with open('README.md', 'rt') as f:
     long_description = f.read()
 
+tests_require = ['factory-boy', 'mock', 'pytest', 'pytest-cov']
 setup(name='hyputils',
-      version='0.0.2',
+      version='0.0.3',
       description='Python utilities for the Hypothes.is REST api and websocket interface',
       long_description=long_description,
       long_description_content_type='text/markdown',
@@ -12,24 +13,32 @@ setup(name='hyputils',
       author='Tom Gillespie',
       author_email='tgbugs@gmail.com',
       license='MIT',
-      classifiers=[],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+      ],
       keywords='hypothesis hypothes.is web annotation',
       packages=['hyputils'],
       python_requires='>=3.6',
+      tests_require=tests_require,
       install_requires=[
           'certifi',
           'requests',
           'websockets',
-          # memex
-          'bleach',
-          'jsonschema',
-          'mistune',
-          'psycopg2',
-          'python-slugify',
-          'sqlalchemy',
-          'webob',
       ],
-      extras_require={'dev':['factory-boy', 'mock', 'pytest', 'pytest-cov', 'pyyaml', 'zdesk']},
+      extras_require={'zdesk': ['pyyaml', 'zdesk'],
+                      'test': tests_require,
+                      'memex':['bleach',
+                               'jsonschema',
+                               'mistune',
+                               'psycopg2',
+                               'python-slugify',
+                               'sqlalchemy',
+                               'webob',
+                              ]
+                     },
       entry_points={
           'console_scripts': [
           ],
