525d2e244d862afe0d08ef3891438152a27d4440
Tom Gillespie <tgbugs@gmail.com>
2019-04-02 05:51:00 -0400

    setup extract version using regext to avoid import errors

--- setup.py
+++ setup.py
@@ -1,11 +1,23 @@
+import re
 from setuptools import setup
+
+
+def find_version(filename):
+    _version_re = re.compile(r"__version__ = '(.*)'")
+    for line in open(filename):
+        version_match = _version_re.match(line)
+        if version_match:
+            return version_match.group(1)
+
+
+__version__ = find_version('hyputils/__init__.py')
 
 with open('README.md', 'rt') as f:
     long_description = f.read()
 
 tests_require = ['factory-boy', 'mock', 'pytest', 'pytest-cov']
 setup(name='hyputils',
-      version='0.0.3',
+      version=__version__,
       description='Python utilities for the Hypothes.is REST api and websocket interface',
       long_description=long_description,
       long_description_content_type='text/markdown',
