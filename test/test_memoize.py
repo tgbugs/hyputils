05fc8f6ff2b62f21165c1bdd5ab0f171b2a2f42a
Tom Gillespie <tgbugs@gmail.com>
2019-10-31 19:51:05 -0700

    test memoize make bad memfile temporary and dev version bump

--- test/test_memoize.py
+++ test/test_memoize.py
@@ -1,32 +1,40 @@
 import unittest
+from pathlib import Path
 from hyputils.hypothesis import Memoizer
 from hyputils.hypothesis import api_token, username, group, group_to_memfile
 
-get_annos = Memoizer(group_to_memfile(group), api_token, username, group)
-
-bad_memfile = '/tmp/test-bad-memfile.json'
-world_get = Memoizer(bad_memfile, api_token, username, '__world__')
-world_annos = world_get.get_annos_from_api(max_results=10)
-world_get.memoize_annos(world_annos)
+bad_memfile = 'test-bad-memfile.json'
 
 
 class TestMemoize(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.get_annos = Memoizer(group_to_memfile(group), api_token, username, group)
+
+        world_get = Memoizer(bad_memfile, api_token, username, '__world__')
+        world_annos = world_get.get_annos_from_api(max_results=10)
+        world_get.memoize_annos(world_annos)
+
+    @classmethod
+    def tearDownClass(cls):
+        Path(bad_memfile).unlink()
+
     def test_0_get_start(self):
-        annos = get_annos.get_annos_from_api(max_results=400)
-        get_annos.memoize_annos(annos)
+        annos = self.get_annos.get_annos_from_api(max_results=400)
+        self.get_annos.memoize_annos(annos)
 
     def test_1_get_file(self):
-        annos, lsu = get_annos.get_annos_from_file()
+        annos, lsu = self.get_annos.get_annos_from_file()
 
     def test_2_add_missin(self):
-        annos, lsu = get_annos.get_annos_from_file()
-        more_annos = get_annos.add_missing_annos(annos, lsu)
+        annos, lsu = self.get_annos.get_annos_from_file()
+        more_annos = self.get_annos.add_missing_annos(annos, lsu)
         assert len(more_annos) > 800 > len(annos)
 
     def test_3_get_rest(self):
-        annos = get_annos.get_annos_from_api(max_results=400)
-        get_annos.memoize_annos(annos)
-        more_annos = get_annos()
+        annos = self.get_annos.get_annos_from_api(max_results=400)
+        self.get_annos.memoize_annos(annos)
+        more_annos = self.get_annos()
         assert len(more_annos) > 800 > len(annos)
 
     def test_4_group_mismatch_at_load_from_file(self):
